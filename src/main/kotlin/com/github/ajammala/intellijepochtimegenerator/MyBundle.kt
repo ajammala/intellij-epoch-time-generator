9c977c213b6b6db10c837b3d54be76974c1ae2fc
GitHub Action <action@github.com>
2022-05-05 22:40:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/ajammala/intellijepochtimegenerator/MyBundle.kt
+++ src/main/kotlin/com/github/ajammala/intellijepochtimegenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ajammala.intellijepochtimegenerator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/ajammala/intellijepochtimegenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ajammala.intellijepochtimegenerator.services
+
+import com.github.ajammala.intellijepochtimegenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
